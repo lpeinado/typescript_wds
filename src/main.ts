33fb5d2ef0a4915159d8eb48a7fd1b6c3f2c6ae7
Lluis Peinado <peinado.l@gmail.com>
2024-01-28 19:39:48 +0100

    feat(main): array types and value/reference

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,10 @@
-import _ from 'lodash';
+const a = [1, 2, 3];
 
+a.push(4);
+a.push('rasque teras');
 
+const dateArray: Date[] = [];
+dateArray.push(new Date());
 
-let a = 'ras que te ras';
-const b = a.charAt(1);
-console.log("B", b);
-
-if(_.isNil(b)=== true) {
-  console.log("B is nil", b);
-}
-else {
-  console.log("B is not nil", b);
-}
+// JS data types by value number, string, null, undefined, boolean
+// JS data types by reference object, array, function (which all of them are objects)
