19cb42ae9d77f8921ffd6294fac4cf0b5f063608
Lluis Peinado <peinado.l@gmail.com>
2024-01-29 20:57:44 +0100

    master(optional): required not after optional

--- src/main.ts
+++ src/main.ts
@@ -1,36 +1,5 @@
-type Address = {
-  street: string;
-  city: string;
-  country: string;
-};
-
-interface Person {
-  firstName: string;
-  lastName: string;
-  age: number;
-  isProgrammer?: boolean;
-  address?: Address;
-};
-
-function logPerson(per:Person){
-  console.log(per.firstName);
+function printNameAndAge(name?: string, age: number) {
+    console.log(name + " is " + age + " years old");
 }
-const lluis = {
-  firstName: 'Lluis',
-  lastName: 'Peinado',
-  age: 25,
-  isProgrammer: true,
-  address: {
-    street: 'Rosselló',
-    city: 'Barcelona',
-    country: 'Catalonia'
-  }
-};
-const a = logPerson(lluis);
-// curious thing here from lesson
-function printPerson(person: {name:string}):void{
-  console.log(person.name);
-}
-printPerson({name: 'Lluisa doesn work with age explicit', age: 25}); // this fails
-const pers = {name: 'LluisaObject works with age', age: 25};
-printPerson(pers); // this works
+printNameAndAge("John", 30);
+printNameAndAge({age:90});
