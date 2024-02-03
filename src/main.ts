98eba2cb8feb7c1ef5a40fa79e1340b4a82b22dc
Lluis Peinado <peinado.l@gmail.com>
2024-02-03 13:43:00 +0100

    master: type as tuple for key values

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,16 @@
-const numsArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] as const;
-numsArray.push(11);
+type Person = {
+  name: string
+  age: number
+}
+const me: Person = {
+  name: 'Jane',
+  age: 25,
+}
+
+
+type Tuple = [string, number| string]
+const tuple: Tuple = ['Jane', 25]
+
+Object.entries(me).forEach(([key, value]: Tuple) => {
+  console.log(`This is a key: ${key}, and this is a value: ${value}`)
+});
