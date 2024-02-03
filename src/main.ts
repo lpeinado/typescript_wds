3ae58f6d6a7667140b5d3b2c44b6a15697bbdc1a
Lluis Peinado <peinado.l@gmail.com>
2024-02-03 15:07:51 +0100

    master: using generic for API responses

--- src/main.ts
+++ src/main.ts
@@ -8,10 +8,18 @@ type Person = {
   age: number,
 }
 type UserData = APIResponse<Person>
+type BlogResponse = APIResponse<{title: string, pages: number}>
 
 const a: UserData = {
   data: {
     name: 'Lluís',
     age: 34
+  },
+}
+
+const b: BlogResponse = {
+  data: {
+    title: 'My blog',
+    pages: 100,
   },
 }
