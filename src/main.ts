274234ab99f921067a4f0a0815e5dc928c846c8e
Lluis Peinado <peinado.l@gmail.com>
2024-02-22 15:50:34 +0100

    master: type narrowing with unknown

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,4 @@
-type Todo = {
-  priority: 'Low' | 'Medium' | 'Hight';
-  description?: string;
-  done: boolean;
-  dueDate: string | Date;
-}
-
-function withTypeGuard (item: Todo){
-  switch(item.priority){
-    case 'Low':
-    console.log(item.priority);  
-    break;
-    case 'Medium':
-      console.log(item.priority);
-      break;
-    case 'Hight':
-      console.log(item.priority);
-      break;
-    default:
-      console.log(item.priority);
-    
-
-  }
+function MyFunction(data:unknown){
+  if(data && data instanceof Object && "name" in data && typeof data.name === 'string')
+  console.log (data.name.length);
 }
